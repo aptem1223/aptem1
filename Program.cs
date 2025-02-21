b51077db62f59d48f76e2762bf84c798bbde22d3
aptem1223 <sanstop357@gmail.com>
2025-02-21 12:21:13 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            edition book1 = new edition("Книга 1", "Автор 1", 2005);
+            edition book2 = new edition("Книга 2", "", 2015);
+            edition book3 = new edition();
+
+            book1.DisplayInfo();
+            book2.DisplayInfo();
+            book3.DisplayInfo();
+
+            book1.ChangeYear(5);
+            book1.DisplayInfo();
+
+            Console.WriteLine(book2.IsYearInRange(2000, 2020));
+        }
+    }
+}
---
+++ edition.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class edition
+    {
+        private string title;
+        private string author;
+        private int year;
+
+        public string Title
+        {
+            get { return string.IsNullOrEmpty(title) ? "неизвестный" : title; }
+            set { title = value; }
+        }
+
+        public string Author
+        {
+            get { return string.IsNullOrEmpty(author) ? "неизвестный" : author; }
+            set { author = value; }
+        }
+
+        public int Year
+        {
+            get { return (year >= 1990 && year <= 2024) ? year : 0; }
+            set { year = (value >= 1990 && value <= 2024) ? value : 0; }
+        }
+
+        public edition() { }
+
+        public edition(string title, string author, int year)
+        {
+            Title = title;
+            Author = author;
+            Year = year;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Название: {Title}, Автор: {Author}, Год издания: {Year}");
+        }
+
+        public void ChangeYear(int years)
+        {
+            year += years;
+            if (year < 1990) year = 1990;
+            if (year > 2024) year = 2024;
+        }
+
+        public bool IsYearInRange(int startYear, int endYear)
+        {
+            return year >= startYear && year <= endYear;
+        }
+    }
+}
