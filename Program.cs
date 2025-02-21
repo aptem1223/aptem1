1f7a6b7af916c8d75a69d6972dd44d852c7a036a
aptem1223 <sanstop357@gmail.com>
2025-02-21 12:41:51 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Software software1 = new Software("ПО 1", "Производитель 1", true);
+            Software software2 = new Software("ПО 2", "Производитель 2", false);
+            Software software3 = new Software();
+
+            software1.DisplayInfo();
+            software2.DisplayInfo();
+            software3.DisplayInfo();
+
+            // Пример смены статуса платности
+            software1.TogglePaidStatus();
+            software1.DisplayInfo();
+
+            // Проверка бесплатности ПО от заданного производителя
+            Console.WriteLine(software2.IsSoftwareFreeFromManufacturer("Производитель 2"));
+        }
+    }
+}
---
+++ Software.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Software
+    {
+        public Software(string v1, string v2, bool v3)
+        {
+        }
+
+        public Software()
+        {
+        }
+
+        class ProgramSoftware
+        {
+            private string name;
+            private string manufacturer;
+            private bool isPaid;
+
+            public string Name
+            {
+                get { return string.IsNullOrEmpty(name) ? "неизвестное ПО" : name; }
+                set { name = value; }
+            }
+
+            public string Manufacturer
+            {
+                get { return string.IsNullOrEmpty(manufacturer) ? "неизвестный производитель" : manufacturer; }
+                set { manufacturer = value; }
+            }
+
+            public bool IsPaid
+            {
+                get { return isPaid; }
+                set { isPaid = value; }
+            }
+
+            public ProgramSoftware() { }
+
+            public ProgramSoftware(string name, string manufacturer, bool isPaid)
+            {
+                Name = name;
+                Manufacturer = manufacturer;
+                IsPaid = isPaid;
+            }
+
+            public void DisplayInfo()
+            {
+                Console.WriteLine($"Название: {Name}, Производитель: {Manufacturer}, {(IsPaid ? "Платное" : "Бесплатное")}");
+            }
+
+            public void TogglePaidStatus()
+            {
+                isPaid = !isPaid;
+            }
+
+            public bool IsSoftwareFreeFromManufacturer(string manufacturer)
+            {
+                return !isPaid && this.manufacturer.Equals(manufacturer, StringComparison.OrdinalIgnoreCase);
+            }
+        }
+
+        internal bool IsSoftwareFreeFromManufacturer(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void TogglePaidStatus()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void DisplayInfo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
